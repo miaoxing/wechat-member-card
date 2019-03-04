60cb27d11c291fe02be374bf157f4f1dc38fd906
twinh <twinhuang@qq.com>
2019-03-04 22:24:54 +0800

    refactoring: 更新会员卡详情页

--- resources/views/wechatMemberCards/detail.php
+++ resources/views/wechatMemberCards/detail.php
@@ -3,39 +3,37 @@
 $view->layout();
 ?>
 
-<div class="container-fluid text-sm bg-white">
-  <div class="ml-3">
-  <div class="row list-header">
-    <h4 class="list-title">
+<div class="container-fluid list list-condensed list-borderless">
+  <div class="row list-item">
+    <div class="col-12 text-lg">
       会员卡详情
-    </h4>
+    </div>
   </div>
-  <div class="row pb-2">
-    <label class="col-3 list-label text-muted">特权说明</label>
-    <div class="col-9 list-content">
+  <div class="row list-item">
+    <div class="col-4 text-muted">特权说明</div>
+    <div class="col-8">
       <?= $e($card['detail']) ?>
     </div>
   </div>
-  <div class="row pb-2">
-    <label class="col-3 list-label text-muted">有效日期</label>
-
-    <div class="col-9 list-content">
+  <div class="row list-item">
+    <label class="col-4 text-muted">有效日期</label>
+    <div class="col-8">
       <?= $card->getDateText() ?>
     </div>
   </div>
   <?php if ($card['service_phone']) : ?>
-    <div class="row pb-2">
-      <div class="col-3 list-label text-muted">电话</div>
-      <div class="col-9 list-content">
+    <div class="row list-item">
+      <div class="col-4 text-muted">电话</div>
+      <div class="col-8">
         <?= $card['service_phone'] ?>
       </div>
     </div>
   <?php endif ?>
   <?php if ($card['business_service']) : ?>
-    <div class="row pb-2">
-      <label class="col-3 list-label text-muted">商户服务</label>
+    <div class="row list-item">
+      <label class="col-4 text-muted">商户服务</label>
 
-      <div class="col-9 list-content">
+      <div class="col-8">
         <?php foreach ($card['business_service'] as $service) : ?>
           <span class="badge badge-success"><?= $serviceNames[$service] ?></span>
         <?php endforeach ?>
@@ -43,12 +41,11 @@ $view->layout();
     </div>
   <?php endif ?>
   <?php if ($card['description']) : ?>
-    <div class="row pb-2">
-      <div class="col-3 list-label text-muted">使用须知</div>
-      <div class="col-9 list-content">
+    <div class="row list-item">
+      <div class="col-4 text-muted">使用须知</div>
+      <div class="col-8">
         <?= $card['description'] ?>
       </div>
     </div>
   <?php endif ?>
-  </div>
 </div>
